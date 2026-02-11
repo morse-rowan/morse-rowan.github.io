0081e92f09f17d48bf278befd9849344ee695ffe
morse-rowan <rowan.morse16@gmail.com>
2026-02-11 18:29:35 -0500

    custom ga event

--- src/pages/PortfolioGoogle.tsx
+++ src/pages/PortfolioGoogle.tsx
@@ -1,6 +1,25 @@
+import { useEffect } from 'react';
 import Portfolio from './Portfolio';
 
+// Declare gtag for TypeScript
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void;
+  }
+}
+
 const PortfolioGoogle = () => {
+  useEffect(() => {
+    // Send custom event to Google Analytics when this page loads
+    if (window.gtag) {
+      window.gtag('event', 'tm_portfolio_view', {
+        page_title: 'Team Matching Portfolio',
+        page_location: window.location.href,
+        page_path: window.location.hash
+      });
+    }
+  }, []);
+
   return <Portfolio />;
 };
 
